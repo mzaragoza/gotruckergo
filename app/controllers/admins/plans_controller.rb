ead0f050d3cf670f94ba3a6357c3aaa9e546561b
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-05 00:19:27 -0400

    admin can controll pricing

---
+++ app/controllers/admins/plans_controller.rb
@@ -0,0 +1,23 @@
+class Admins::PlansController < AdminController
+  expose(:plans){ Plan.order("sort_order").scoped{} }
+  expose(:plan)
+
+  def create
+    if plan.save
+      flash[:notice] = t(:plan_was_successfully_created)
+      redirect_to(admins_plans_path)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if plan.save
+      flash[:notice] = t(:plan_was_successfully_updated)
+      redirect_to(admins_plans_path)
+    else
+      render :edit
+    end
+  end
+end
+
