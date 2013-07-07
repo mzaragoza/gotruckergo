4cc40dffc82558908700715fac7b167c6c03d0dc
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-06 22:37:57 -0400

    test driver photo

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -21,7 +21,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process :resize_to_fit => [800, 800]
 
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [50, 50]
   end
 
 end
