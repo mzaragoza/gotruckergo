3c35339287c0eb5c20d1f98d6bb13db47f0e9557
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-06 22:35:37 -0400

    put traile back on shipment

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -18,6 +18,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
      %w(jpg jpeg gif png)
   end
 
+  process :resize_to_fit => [800, 800]
+
   version :thumb do
     process :resize_to_limit => [200, 200]
   end
